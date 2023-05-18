fc32edb374cee5fc408818d54e9b3a00eb5cc848
RawJrk <rodjer7777@gmail.com>
2023-05-18 11:34:14 +0300

    moved options to separate json file

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ import { Footer, Link } from "./components/Footer";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./styles/theme";
 import GlobalStyles from "./styles/global";
-
-const options = [
-  { title: "Binary [base-2]", value: 2 },
-  { title: "Octal [base-8]", value: 8 },
-  { title: "Decimal [base-10]", value: 10 },
-  { title: "Hexadecimal [base-16]", value: 16 },
-];
+import options from "./options.json";
 
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useCachedState("darktheme", false);
