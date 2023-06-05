8a337505605c02fe53f19126ae672c29fa3fbf7a
RawJrk <rodjer7777@gmail.com>
2023-06-05 18:50:22 +0300

    moved form-related components to a separate folder

--- src/feature/shared/index.js
+++ src/feature/shared/index.js
@@ -1,10 +1,6 @@
-import Form from "./components/Form";
-import Label from "./components/Label";
-import Input from "./components/Input";
-import Select from "./components/Select";
-import Options from "./components/Options";
 import Button from "./components/Button";
 import Footer from "./components/Footer";
 import Link from "./components/Link";
 
-export { Form, Label, Input, Select, Options, Button, Footer, Link };
+export { Button, Footer, Link };
+export { Form, Label, Input, Select, Options } from "./components/Form";
